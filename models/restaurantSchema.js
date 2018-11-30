e1903f5fd8f650ccc2a08d4f4760cf7a61164da8
cheungwanyu <cheungwanyu@gmail.com>
2018-11-30 21:47:25 +0800

    restaurantSchema

---
+++ models/restaurantSchema.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var restaurantSchema = mongoose.Schema({
+	restaurant_id: String,
+	name: String,
+	borough: String,
+	photo:Buffer,
+	photo_nimetype: String,
+	address : {
+		 street: String,
+		 zipcode: String,
+		 building: String,
+		 coord: [Number,Number]
+		 },
+	grades: [{grade: String, score: Number}],
+	onwer: String
+});
+
+module.exports = restaurantSchema;
+restaurant.js
+Displaying restaurant.js.
